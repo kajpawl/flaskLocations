7d43b03c6fa9f98cb33c9672d39b114c2f376768
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-10-24 21:16:10 +0200

    Add signup form route and view

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug import generate_password_hash, check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
--- routes.py
+++ routes.py
@@ -2,8 +2,9 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from models import db
+from forms import SignupForm
 
 base_dir = Path(__file__).resolve().parent
 env_file = base_dir / '.env'
@@ -14,15 +15,27 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 db.init_app(app)
 
+app.secret_key = os.environ.get('SECRET_KEY')
 
-@app.route("/")
+
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
 
-@app.route("/about")
+@app.route('/about')
 def about():
-    return render_template("about.html")
+    return render_template('about.html')
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+
+    if request.method == 'POST':
+        return 'Success!'
+    elif request.method == 'GET':
+        return render_template('signup.html', form=form)
 
 
 if __name__ == '__main__':
