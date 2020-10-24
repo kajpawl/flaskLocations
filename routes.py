dc70980a95e11bf1277af3fb35c57ee8b8b717aa
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-10-24 20:51:52 +0200

    Connect to DB and add User model

--- routes.py
+++ routes.py
@@ -1,6 +1,18 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
 from flask import Flask, render_template
+from models import db
+
+base_dir = Path(__file__).resolve().parent
+env_file = base_dir / '.env'
+load_dotenv(env_file)
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+db.init_app(app)
 
 
 @app.route("/")
