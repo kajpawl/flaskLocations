f8bb41cf52ded7d0f84c0602bfc2f8077bef50ff
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-10-25 13:08:30 +0100

    Add check for existing user email

--- routes.py
+++ routes.py
@@ -2,8 +2,8 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-from flask import Flask, render_template, request
-from models import db
+from flask import Flask, render_template, request, abort
+from models import db, User
 from forms import SignupForm
 
 base_dir = Path(__file__).resolve().parent
@@ -33,9 +33,17 @@ def signup():
     form = SignupForm()
 
     if request.method == 'POST':
-        if form.validate() == False:
+        if not form.validate():
             return render_template('signup.html', form=form)
         else:
+            new_user = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
+
+            user_with_existing_email = User.query.filter(User.email == form.email.data).first()
+            if user_with_existing_email is not None:
+                abort(409, description=f'User with email {form.email.data} already exists.')
+
+            db.session.add(new_user)
+            db.session.commit()
             return 'Success!'
     elif request.method == 'GET':
         return render_template('signup.html', form=form)
