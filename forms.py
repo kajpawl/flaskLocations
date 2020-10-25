59be04b91176bdb5aaf12c29e31449164870892b
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-10-25 12:54:19 +0100

    Add signup form validation

--- forms.py
+++ forms.py
@@ -1,10 +1,14 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
 
 
 class SignupForm(Form):
-    first_name = StringField('First name')
-    last_name = StringField('Last name')
-    email = StringField('Email')
-    password = PasswordField('Password')
+    first_name = StringField('First name', validators=[DataRequired("Please enter your first name.")])
+    last_name = StringField('Last name', validators=[DataRequired("Please enter your last name.")])
+    email = StringField('Email', validators=[DataRequired("Please enter your email address."),
+                                             Email("Please make sure that email format is correct.")])
+    password = PasswordField('Password', validators=[DataRequired("Please enter your password."),
+                                                     Length(min=6, max=54,
+                                                            message="Password must have 6 to 54 characters.")])
     submit = SubmitField('Sign up')
